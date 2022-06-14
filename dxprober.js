ae1f97592b00a2b9fd2edc2e288d8bad43c0cef8
AlfredliuX <liuxin2016@gmail.com>
2022-06-14 13:50:31 +0800

    feat: 加快上传速度
    启用 ajax 的异步请求以加快成绩上传速度
    完成后的提示信息改为在 html 中呈现（而不是 alert ）

--- dxprober.js
+++ dxprober.js
@@ -1,23 +1,35 @@
-function onUploadStart() {
-    document.getElementById("dxp-btn").setAttribute("disabled", "disabled");
-    document.getElementById("loadingimg").style.display = "block";
-}
-function onUploadEnd() {
-    document.getElementById("dxp-btn").removeAttribute("disabled");
-    document.getElementById("loadingimg").style.display = "none";
+function doUpload() {
+    [0, 1, 2, 3, 4].forEach((diff) => {
+        $.ajax({
+            url: "https://maimai.wahlap.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=" + diff,
+            type: "GET",
+            async: false,
+            success: (res) => {
+                $.ajax({
+                    url: "https://www.diving-fish.com/api/pageparser/page",
+                    type: "POST",
+                    async: true,
+                    data: "<login><u>" + window.u + "</u><p>" + window.p + "</p></login>" + res.match(/<html.*>([\s\S]*)<\/html>/)[1].replace(/\s+/g, " "),
+                    contentType: "text/plain",
+                    success: (res) => document.getElementById("info-box").innerHTML += "diff" + diff + ": " + JSON.stringify(res) + "<br>"
+                });
+            }
+        });
+    });
 }
 
 window.onload = function () {
-    var loadingimg = document.createElement("img");
-    loadingimg.id = "loadingimg";
-    loadingimg.src = "https://maimai.pages.dev/loading.gif";
-    loadingimg.style = "border-radius:4px;position:fixed;bottom:1.4rem;right:1.4rem;width:3rem;height:3rem;z-index:514;\
-    display:none;background-color: #33333333;padding:0.3rem;";
-    document.body.appendChild(loadingimg);
+    var box = document.createElement("div");
+    box.id = "info-box";
+    box.style = "position:fixed;bottom:4.8rem;right:1.4rem;z-index:514;overflow:scroll;\
+    width:calc(100% - 2.8rem);height:10rem;max-width:320px;border-radius:4px;\
+    padding:1rem;background-color:#ffffffbb;box-shadow:1px 2px 6px rgba(0,0,0,0.2);\
+    backdrop-filter: blur(5px);-webkit-backdrop-filter: blur(5px);";
+    box.innerHTML = "";
 
     var button = document.createElement("Button");
     button.id = "dxp-btn"
-    button.innerHTML = "上传</br>成绩";
+    button.innerHTML = "上传<br>成绩";
     button.style = "width:3rem;height:3rem;border-radius:4px;bottom:1.4rem;right:1.4rem;position:fixed;z-index: 114;\
     background-color: #ffffffbb;box-shadow:1px 2px 6px rgba(0,0,0,0.2);\
     backdrop-filter: blur(5px);-webkit-backdrop-filter: blur(5px);\
@@ -25,27 +37,10 @@ window.onload = function () {
     button.onclick = function () {
         var r = confirm("确定上传成绩到查分器吗？\n(上传需要一点时间，请勿离开页面)");
         if (r == true) {
-            onUploadStart();
-            c = "";
-            [0, 1, 2, 3, 4].forEach((diff) => {
-                $.ajax({
-                    url: "https://maimai.wahlap.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=" + diff,
-                    type: "GET",
-                    async: false,
-                    success: (res) => {
-                        $.ajax({
-                            url: "https://www.diving-fish.com/api/pageparser/page",
-                            type: "POST",
-                            async: false,
-                            data: "<login><u>" + window.u + "</u><p>" + window.p + "</p></login>" + res.match(/<html.*>([\s\S]*)<\/html>/)[1].replace(/\s+/g, " "),
-                            contentType: "text/plain",
-                            success: (res) => window.c += "diff" + diff + ": " + res.message + "\n"
-                        });
-                    }
-                });
-            });
-            alert(window.c);
-            onUploadEnd();
+            document.body.appendChild(box);
+            document.getElementById("dxp-btn").setAttribute("disabled", "disabled");
+            doUpload();
+            document.getElementById("dxp-btn").removeAttribute("disabled");
         }
     }
     document.body.appendChild(button);
